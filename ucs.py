05412c1d1e71475dd021bc5eb991c1c7ea8b3859
2023000080 <scheruku6@gitam.in>
2026-02-19 22:16:23 +0530

    Add files via upload

---
+++ ucs.py
@@ -0,0 +1,32 @@
+import heapq
+
+def uniform_cost_search(graph, start, goal):
+    priority_queue = [(0, start)]
+    visited = {}
+
+    while priority_queue:
+        cost, node = heapq.heappop(priority_queue)
+
+        if node in visited:
+            continue
+
+        visited[node] = cost
+
+        if node == goal:
+            return cost
+
+        for neighbour, edge_cost in graph[node]:
+            heapq.heappush(priority_queue, (cost + edge_cost, neighbour))
+
+    return float('inf')
+
+
+graph = {
+    'A': [('B', 1), ('C', 4)],
+    'B': [('C', 2), ('D', 5)],
+    'C': [('D', 1)],
+    'D': []
+}
+
+print("Minimum cost to reach D:",
+      uniform_cost_search(graph, 'A', 'D'))
